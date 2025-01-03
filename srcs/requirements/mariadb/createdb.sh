19453681a0ea4fc0acd0304a53f6f50fec0efb7a
ftanon <ftanon@ftanon.ftanon>
2025-01-03 13:37:06 +0100

    mariadb volume and network

--- srcs/requirements/mariadb/createdb.sh
+++ srcs/requirements/mariadb/createdb.sh
@@ -2,4 +2,5 @@
 sed -i "s/\:DB_NAME/${DB_NAME}/g" /etc/mysql/init.sql
 sed -i "s/\:DB_USER/${DB_USER}/g" /etc/mysql/init.sql
 sed -i "s/\:DB_PASSWORD/${DB_PASSWORD}/g" /etc/mysql/init.sql
+mysql_install_db
 mysqld
