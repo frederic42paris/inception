6c5912260f57620e9d02a6463849eb78b2e11271
ftanon <ftanon@ftanon.ftanon>
2025-01-05 18:02:08 +0100

    cleaning

--- srcs/requirements/mariadb/tools/createdb.sh
+++ srcs/requirements/mariadb/tools/createdb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sed -i "s/\:DB_NAME/${DB_NAME}/g" /etc/mysql/init.sql
-sed -i "s/\:DB_USER/${DB_USER}/g" /etc/mysql/init.sql
-sed -i "s/\:DB_PASSWORD/${DB_PASSWORD}/g" /etc/mysql/init.sql
+sed -i "s/\:MARIADB_DB_NAME/${MARIADB_DB_NAME}/g" /etc/mysql/init.sql
+sed -i "s/\:MARIADB_USERNAME/${MARIADB_USERNAME}/g" /etc/mysql/init.sql
+sed -i "s/\:MARIADB_PASSWORD/${MARIADB_PASSWORD}/g" /etc/mysql/init.sql
+
 mysql_install_db
 mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -3,9 +3,9 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
-./wp-cli.phar core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
-./wp-cli.phar user create $DB_USER fred@gmail.com --user_pass=$DB_PASSWORD --allow-root
+./wp-cli.phar config create --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USERNAME --dbpass=$MARIADB_PASSWORD --dbhost=$MARIADB_HOST --allow-root
+./wp-cli.phar core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USERNAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+./wp-cli.phar user create $WP_USER_USERNAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --allow-root
 
 
 php-fpm7.4 -F
