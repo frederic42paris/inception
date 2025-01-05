7e1c068633c548bdd61b3773235312702c97d943
ftanon <ftanon@ftanon.ftanon>
2025-01-05 17:23:27 +0100

    2 users wordpress database

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,5 +5,7 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
 ./wp-cli.phar core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
+./wp-cli.phar user create $DB_USER fred@gmail.com --user_pass=$DB_PASSWORD --allow-root
+
 
 php-fpm7.4 -F
